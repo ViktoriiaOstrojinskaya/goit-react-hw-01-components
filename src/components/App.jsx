8af2354e691cc17b84f57445382122074a4509d9
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-11-20 20:59:51 -0500

    task #1-4 react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
+import { Profile } from 'components/Profile/Profile';
+import user from '../user.json';
+import { Statistics } from 'components/Statistics/Statistics';
+import data from '../data.json';
+import { FriendList } from 'components/FriendList/FriendList';
+import friends from '../friends.json';
+import { TransactionHistory } from 'components/TransactionHistory/Transactions';
+import transactions from '../transactions.json';
+
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        height: '1200px',
+        height: '1200px',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
+
+// App.PropTypes = {
+//   Profile: PropTypes.string,
+// };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(friend => (
+        <li className="item" key={friend.id}>
+          <span className="status">{friend.isOnline}</span>
+          <img
+            className="avatar"
+            src={friend.avatar}
+            alt="User avatar"
+            width="48"
+          />
+          <p className="name">{friend.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(PropTypes.shape),
+};
